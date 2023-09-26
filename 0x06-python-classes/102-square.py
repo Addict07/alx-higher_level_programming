1a61a6d574b18016b59cac31aa11ba03d92c624d
Michael <addictedchrysler@gmail.com>
2023-09-26 11:00:51 -0700

    ddone

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+class Square:
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def __eq__(self, other):
+        """ the == comparision to a Square."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """the != comparison to a Square."""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """ the < comparison to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """the <= comparison to a Square."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """the >= compmarison to a Square."""
+        return self.area() >= other.area()
